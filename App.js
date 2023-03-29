3b81fb6b83769591babb9bd2b0fe52591add6023
sanjana <sanjana.advani@neosoftmail.com>
2023-03-29 16:52:32 +0530

    files added to navigator

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
+import 'react-native-gesture-handler';
 import React from 'react'
 import Basic from './screens/Basic'
 import Address from './screens/Address';
+import Professional from './screens/Professional'
+import {NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 export default function App() {
   return (
-  <Address/>
+    <NavigationContainer>
+      <Stack.Navigator>
+    <Stack.Screen name="Basic" component={Basic} />
+    <Stack.Screen name="Professional" component={Professional} />
+    <Stack.Screen name="Address" component={Address} />
+  </Stack.Navigator>
+    </NavigationContainer>
+    
   );
 }
